026bc11ba660659f78c80e59a2d27740c7bf7cf8
A06 <160205029+Alishba06@users.noreply.github.com>
2024-05-10 16:13:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//02 Question:
+var name1 = "alishba";
+console.log("Hello, $(name1)would you like to learn some Codeing today");
+//03 Question: 
+var name2 = "alishba";
+console.log(name2.toLowerCase());
+console.log(name2.toUpperCase());
+console.log(name2.charAt(0).toUpperCase() + name2.slice(1).toLowerCase());
+//04 Question:
---
+++ index.ts
@@ -0,0 +1,16 @@
+//02 Question:
+
+let name1: string = "alishba"
+
+console.log(`Hello, $(name1)would you like to learn some Codeing today`)
+
+//03 Question: 
+
+let name2: string = "alishba";
+
+console.log(name2.toLowerCase());
+console.log(name2.toUpperCase());
+console.log(name2.charAt(0).toUpperCase () + name2.slice (1).toLowerCase());
+
+//04 Question:
+
